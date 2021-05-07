0364750e4af3ed61f2468834bef6a4b89dcb63c3
mdobraz <mdobraz@gmail.com>
2021-05-07 10:56:28 +0200

    Update make_surfaces.sh

--- make_surfaces.sh
+++ make_surfaces.sh
@@ -5,12 +5,12 @@ FSLPREFIX="fsl5.0-"
 mkdir surfaces
 OUTp="surfaces/"
 
-IN="In-sub-EloukMION_ses-01_T1w0p4mm_denoised_debiased_in-inia19_sub-EloukMION_res-GLM_denoise_macaque_vs_all_t"
+IN="In-sub-Maga_ses-01_mp2rageT1w_denoised_debiased_in-inia19_sub-Maga_res-7WM_4CSF_0mvt_macaque_vs_all_t"
 cIN=$OUTp$IN
 
 "${FSLPREFIX}fslmaths" $IN $cIN -odt short
-# "${FSLPREFIX}fslmaths" $cIN -mul -1 $cIN -odt short
-# "${FSLPREFIX}fslmaths" $cIN -thr 6 -bin $cIN -odt short
+"${FSLPREFIX}fslmaths" $cIN -mul -1 $cIN -odt short
+"${FSLPREFIX}fslmaths" $cIN -thr 5 -bin $cIN -odt short
 
 "${BVp}AimsMesh" -i $cIN
 SUFF="_1_0.gii" # suffix of the surface that you want to smooth
